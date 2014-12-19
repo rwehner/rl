85f98e47d2c86739e8fed0b6e1d7f6ada8290d56
Robert Wehner <robert.wehner@returnpath.com>
2014-12-19 15:12:20 -0700

    Solved all problems in Warmup1.

--- codingbat/python/Warmup1.py
+++ codingbat/python/Warmup1.py
@@ -27,50 +27,73 @@ def sum_double(a, b):
     Given two int values, return their sum. Unless the two values are the same, then
     return double their sum.
     '''
-    pass
+    if a != b:
+        return a + b
+    return 2 * (a + b)
 
 def diff21(n):
     '''
     Given an int n, return the absolute difference between n and 21, except return
     double the absolute difference if n is over 21.
     '''
-    pass
+    if n > 21:
+        return 2 * (abs(n) - 21)
+    return 21 - abs(n)
 
 def parrot_trouble(talking, hour):
-
     '''
     We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
     We are in trouble if the parrot is talking and the hour is before 7 or after 20.
     Return True if we are in trouble.
     '''
-    pass
+    if talking and (hour < 7 or hour > 20):
+        return True
+    return False
+
 
 def makes10(a, b):
     '''
     Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
     '''
-    pass
+    if a == 10 or b == 10 or a + b == 10:
+        return True
+    return False
 
 def near_hundred(n):
     '''
     Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute
     value of a number.
     '''
-    pass
+    if (90 <= abs(n) <= 110) or (190 <= abs(n) <= 210):
+        return True
+    return False
+
 
 def pos_neg(a, b, negative):
     '''
     Given 2 int values, return True if one is negative and one is positive. Except if the parameter
     "negative" is True, then return True only if both are negative.
     '''
-    pass
+    if negative:
+        if a < 0 and b < 0:
+            return True
+        else:
+            return False
+
+    if (a < 0 and b > 0) or (a > 0 and b < 0):
+        return True
+    else:
+        return False
 
 def not_string(str):
     '''
     Given a string, return a new string where "not " has been added to the front. However, if the
     string already begins with "not", return the string unchanged.
     '''
-    pass
+    if str.startswith('not '):
+        return str
+    else:
+        return 'not %s' % str
 
 def missing_char(str, n):
     '''
@@ -78,19 +101,28 @@ def missing_char(str, n):
     The value of n will be a valid index of a char in the original string (i.e. n will be in the range
     0..len(str)-1 inclusive).
     '''
-    pass
+    return str[:n] + str[n+1:]
 
 def front_back(str):
     '''
     Given a string, return a new string where the first and last chars have been exchanged.
     '''
-    pass
+    if len(str) > 2:
+        return "%s%s%s" % (str[-1], str[1:-1], str[0])
+    elif len(str) == 2:
+        return "%s%s" % (str[-1], str[0])
+    else:
+        return str
+
 
 def front3(str):
     '''
     Given a string, we'll say that the front is the first 3 chars of the string. If the string
     length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
     '''
-    pass
+    if len(str) < 3:
+        return str * 3
+    else:
+        return str[:3] * 3
 
 
