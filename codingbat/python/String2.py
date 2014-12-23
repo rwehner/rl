963472682c4198331b297239c79fbbe5a1a0749c
Robert Wehner <robert.wehner@returnpath.com>
2014-12-23 16:29:49 -0700

    Finish String2 exercises.

--- codingbat/python/String2.py
+++ codingbat/python/String2.py
@@ -10,29 +10,41 @@ def double_char(str):
     Given a string, return a string where for every char in the original,
     there are two chars.
     '''
-    pass
+    return ''.join([ch + ch for ch in str])
 
 def count_hi(str):
     '''
     Return the number of times that the string "hi" appears anywhere in
     the given string.
     '''
-    pass
+    return str.count('hi')
 
 def cat_dog(str):
     '''
     Return True if the string "cat" and "dog" appear the same number of times
     in the given string.
     '''
-    pass
+    return str.count('dog') ==  str.count('cat')
 
 def count_code(str):
     '''
     Return the number of times that the string "code" appears anywhere in the
     given string, except we'll accept any letter for the 'd', so "cope" and
     "cooe" count.
+    regex is disallowed  :(
+    import re
+    return len(re.findall("co.e", str))
     '''
-    pass
+    if 'co' not in str or len(str) < 4:
+        return 0
+
+    count = 0
+    for index, character in enumerate(str):
+        if character == 'c':
+            word = str[index:index + 4]
+            if word.startswith('co') and word.endswith('e'):
+                count += 1
+    return count
 
 def end_other(a, b):
     '''
@@ -41,12 +53,23 @@ def end_other(a, b):
     other words, the computation should not be "case sensitive").
     Note: s.lower() returns the lowercase version of a string.
     '''
-    pass
+    return a.lower().endswith(b.lower()) or b.lower().endswith(a.lower())
 
 def xyz_there(str):
     '''
     Return True if the given string contains an appearance of "xyz" where the
-    xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz"
+    xyz is not directly preceded by a period (.). So "xxyz" counts but "x.xyz"
     does not.
     '''
-    pass
+    if 'xyz' not in str:
+        return False
+
+    remaining = str[:]
+    next_index = remaining.find('xyz')
+    while next_index >= 0:
+        if next_index == 0 or remaining[next_index - 1] != '.':
+            return True
+        else:
+            remaining = remaining[next_index + 3:]
+            next_index = remaining.find('xyz')
+    return False
--- codingbat/python/test_String2.py
+++ codingbat/python/test_String2.py
@@ -5,32 +5,32 @@ Tests for codingbat.com's String-2 Python problems
 import pytest
 from String2 import *
 
-def test_double_char(str):
+def test_double_char():
     assert double_char('The') == 'TThhee'
     assert double_char('AAbb') == 'AAAAbbbb'
     assert double_char('Hi-There') == 'HHii--TThheerree'
 
-def test_count_hi(str):
+def test_count_hi():
     assert count_hi('abc hi ho') == 1
     assert count_hi('ABChi hi') == 2
     assert count_hi('hihi') == 2
 
-def test_cat_dog(str):
+def test_cat_dog():
     assert cat_dog('catdog') == True
     assert cat_dog('catcat') == False
     assert cat_dog('1cat1cadodog') == True
 
-def test_count_code(str):
+def test_count_code():
     assert count_code('aaacodebbb') == 1
     assert count_code('codexxcode') == 2
     assert count_code('cozexxcope') == 2
 
-def test_end_other(a, b):
+def test_end_other():
     assert end_other('Hiabc', 'abc') == True
     assert end_other('AbC', 'HiaBc') == True
     assert end_other('abc', 'abXabc') == True
 
-def test_xyz_there(str):
+def test_xyz_there():
     assert xyz_there('abcxyz') == True
     assert xyz_there('abc.xyz') == False
     assert xyz_there('xyz.abc') == True
