1bd0a16ffab07f72b3709e7ab96ec6c888d7b617
Robert Wehner <robert.wehner@returnpath.com>
2015-01-17 11:27:37 -0700

    Add in list1 exercises and solutions

--- codingbat/python/List1.py
+++ codingbat/python/List1.py
@@ -20,34 +20,80 @@ def same_first_last(nums):
     else:
         return False
 
-"""
-def
+def make_pi():
     '''
+    Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+    '''
+    return [3, 1, 4]
+
+def common_end(a, b):
+    '''
+    Given 2 arrays of ints, a and b, return True if they have the same first 
+    element or they have the same last element. Both arrays will be length 1 or more.
+    '''
+    return (a[0] == b[0] or a[-1] == b[-1])
+
+def sum3(nums):
+    '''
+    Given an array of ints length 3, return the sum of all the elements.
+    '''
+    return sum(nums)
+
+def rotate_left3(nums):
+    '''
+    Given an array of ints length 3, return an array with the elements "rotated left" 
+    so {1, 2, 3} yields {2, 3, 1}.
+    '''
+    return nums[1:] + [nums[0]]
+
+def reverse3(nums):
+    '''
+    Given an array of ints length 3, return a new array with the elements in reverse order, 
+    so {1, 2, 3} becomes {3, 2, 1}.
+    '''
+    nums.reverse()
+    return nums
+
+def max_end3(nums):
     
     '''
-    pass
+    Given an array of ints length 3, figure out which is larger between the first and last 
+    elements in the array, and set all the other elements to be that value. Return the changed array.
+    '''
+    largest = max(nums)
+    return [largest] * len(nums)
 
-def
+def sum2(nums):
     '''
-    
+    Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, 
+    just sum up the elements that exist, returning 0 if the array is length 0.
     '''
-    pass
+    length = len(nums)
+    if length == 0:
+        return 0
+    elif length < 2:
+        return sum(nums)
+    else:
+        return sum(nums[:2])
 
-def
+def middle_way(a, b):
     '''
-    
+    Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
     '''
-    pass
+    return [a[1], b[1]]
 
-def
+def make_ends(nums):
+    '''
+    Given an array of ints, return a new array length 2 containing the first and last elements from the original array. 
+    The original array will be length 1 or more. 
+    '''
+    return [nums[0], nums[-1]]
+
+def has23(nums):
     '''
 
+    Given an int array length 2, return True if it contains a 2 or a 3. 
     '''
-    pass
+    return (2 in nums or 3 in nums)
 
-def
-    '''
 
-    '''
-    pass
-"""
